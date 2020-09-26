073bfd6e4caf24500fab833a249b6aff68a4f06b
张毅哲 <1061127386@qq.com>
2020-09-26 18:43:11 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+html{
+  font-size: 62.5%;
+}
+html,body{
+  height: 100%;
+  width: 100%;
+}
+#app{
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+        <p>login</p>
+
+        <section>login</section>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+
+ 
+        }
+    }
+}
+</script>
+ 
+<style lang = "less" scoped>
+p{
+    font-size:1.6rem;
+}
+section{
+    font-size: 16px;
+}
+</style>
